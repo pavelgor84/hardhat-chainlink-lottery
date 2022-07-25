4ce5e001dbf0071e3e01c0e762e3342b93152867
pavelgor84 <pavelgor@gmail.com>
2022-07-25 10:03:20 +0400

    VRFCoordinator condition

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,9 +1,19 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
+const { developementNetworks, networkConfig } = require("../helper-hh-config")
 
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts
+    const chainId = network.config.chainId
+    let VRFCoordinatorV2
+
+    if (developementNetworks.includes(network.name)) {
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        VRFCoordinatorV2 = VRFCoordinatorV2Mock.address // check this <------
+    } else {
+        VRFCoordinatorV2 = networkConfig.chainId["vrfCoordinatorV2"]
+    }
 
     const raffle = deploy("Raffle", {
         from: deployer,
