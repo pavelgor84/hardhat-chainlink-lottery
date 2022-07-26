995a3d26052fc1661ffb7f9ec4af8041b94d385a
pavelgor84 <pavelgor@gmail.com>
2022-07-26 14:26:34 +0400

    enterRaffle test

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,14 +1,14 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developementNetworks } = require("../../helper-hh-config")
 const { networkConfig } = require("../../helper-hh-config")
 
 !developementNetworks.includes(network.name) ? describe.skip : describe("Unit tests:", function () {
-    let raffle, vrfCoordinatorV2
+    let raffle, vrfCoordinatorV2, deployer
     const chainId = network.config.chainId
 
     beforeEach(async () => {
-        const { deployer } = await getNamedAccounts()
+        deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
 
         vrfCoordinatorV2 = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
@@ -22,6 +22,18 @@ const { networkConfig } = require("../../helper-hh-config")
             const interval = await raffle.getInterval()
             assert.equal(status.toString(), "0")
             assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+        })
+    })
+
+    describe("enterRaffle", function () {
+        it("should revert on low entrance fee", async () => {
+            await expect(raffle.enterRafle()).to.be.revertedWith("Raffle__notEnoughFeeToEnter")
+        })
+        it("should add player to array of players", async () => {
+            const getEntranceFee = await raffle.getEntranceFee()
+            const enter = await raffle.enterRaffle({ value: getEntranceFee })
+            const player = await raffle.getPlayer(0)
+            assert.equal(player, deployer)
         })
     })
 
